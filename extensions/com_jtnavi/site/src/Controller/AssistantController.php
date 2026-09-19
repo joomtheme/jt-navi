49ce03dd4d4ef05664fa94c917a89bf26485620c
Ugur Uygur <support@joomtheme.com>
2026-09-19 11:23:17 +0300

    Add explicit GPL v2-or-later notices to all PHP file headers

--- extensions/com_jtnavi/site/src/Controller/AssistantController.php
+++ extensions/com_jtnavi/site/src/Controller/AssistantController.php
@@ -3,6 +3,19 @@
  * @package     JT Navi
  * @copyright   (C) 2026 JoomTheme. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 namespace JoomTheme\Component\JtNavi\Site\Controller;
 
--- extensions/mod_jtnavi/src/Helper/SuggestionsHelper.php
+++ extensions/mod_jtnavi/src/Helper/SuggestionsHelper.php
@@ -1,5 +1,22 @@
 <?php
-/** @package JT Navi @copyright (C) 2026 JoomTheme. @license GPL-2.0-or-later */
+/**
+ * @package     JT Navi
+ * @copyright   (C) 2026 JoomTheme. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 namespace JoomTheme\Module\JtNavi\Site\Helper;
 defined('_JEXEC') or die;
 use Joomla\Registry\Registry;
